bedf963975b2ee3fe0f906ff4f62a2da9a936cb9
Nigel Cheok <cheok.nigel@gmail.com>
2019-07-29 19:11:03 +0800

    Updated Card

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Avatar(props) {
   return (
-    <a href={props.link} target="_blank">
       <div className="card border-square mt-3 flex-grow-1 mr-3" style={AvatarStyle()}>
-        <div className="card-body">
+        <a href={props.link} target="_blank" style={AnchorStyle()}>
+        <div className="card-body" style={AvatarBodyStyle()}>
           <img src={props.avatarUrl} style={AvatarImageStyle()} height="60" width="60" alt={props.name}/>
           <div className="mt-2" style={AvatarTitleStyle()}>
             { props.name }
@@ -13,16 +13,27 @@ export function Avatar(props) {
             { props.description }
           </div>
         </div>
+        </a>
       </div>
-    </a>
   );
 };
+
+function AnchorStyle() {
+  return {
+    textDecoration: 'none'
+  }
+}
 
 function AvatarStyle() {
   return {
     minWidth: '350px',
     backgroundColor: 'rgba(37,39,41,.01)',
     textAlign: 'center',
+  }
+}
+
+function AvatarBodyStyle() {
+  return {
     textDecoration: 'none !important',
     color: 'rgba(37,39,41)'
   }
