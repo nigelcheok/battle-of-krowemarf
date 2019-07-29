ee77c44dae59685f55edde899ab6c73087bd30fe
Nigel Cheok <cheok.nigel@gmail.com>
2019-07-29 14:48:42 +0800

    Updated styling

--- src/Components/Button/ExternalLinkButton/ExternalLinkButton.jsx
+++ src/Components/Button/ExternalLinkButton/ExternalLinkButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function ExternalLinkButton(props) {
   return (
     <a href={props.link} target="_blank">
-      <button type="button" className="btn btn-dark font-weight-bold">
+      <button type="button" className="btn btn-outline-dark font-weight-bold">
         <div className={`fab ${props.icon} mr-2`}/>
         { props.text }
       </button>
