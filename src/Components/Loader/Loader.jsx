ecf1dd407e3f680c957cf0607e80d8f950196cd5
Nigel Cheok <cheok.nigel@gmail.com>
2019-07-30 10:38:38 +0800

    added loader error statement

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function Loader() {
+  const [isLoading, setLoader] = useState(true);
+
+  useEffect( () => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="spinner-grow text-secondary" style={LoaderStyle()} role="status">
-      <span className="sr-only">Loading...</span>
+    <div>
+      { isLoading &&
+        <div className="spinner-grow text-secondary" style={LoaderStyle()} role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      }
+      { !isLoading &&
+        <div className="mt-4" style={LoadingTextStyle()}>Sorry, there's an error loading.</div>
+      }
     </div>
   );
 }
@@ -12,5 +27,11 @@ function LoaderStyle() {
   return {
     width: '5rem',
     height: '5rem',
+  }
+}
+
+function LoadingTextStyle() {
+  return {
+    fontSize: '1.25rem',
   }
 }
