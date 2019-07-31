7d732aa9ebad360ecda7fc59125c2769e6c5e642
Nigel Cheok <cheok.nigel@gmail.com>
2019-07-31 22:19:35 +0800

    reduce height of lazy loader

--- src/Components/Page/PageHeader/PageHeader.js
+++ src/Components/Page/PageHeader/PageHeader.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ClanConstants from '../../../Constants/ClanConstants';
 import { SectionHeader } from '../../Section/SectionHeader/SectionHeader';
 import { TeamToggleButton } from '../../Button/TeamToggleButton/TeamToggleButton';
